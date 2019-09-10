5089f27c9a6dfd648c1ae708de883b47b7a5c2e7
Joseph Harburg <josephharburg@gmail.com>
2019-09-10 13:50:33 -0400

    fixed bugs

--- GCDeliverableTwoJH/Program.cs
+++ GCDeliverableTwoJH/Program.cs
@@ -24,49 +24,65 @@ namespace GCDeliverableTwoJH
                     {
                         case (1):
                             Output(1, "go stock car racing");
+                            wrongResponse = false;
                             break;
                         case (2):
                             Output(2, "go hiking");
+                            wrongResponse = false;
                             break;
                         case (3):
                             Output(3, "go Skydiving");
+                            wrongResponse = false;
                             break;
                         case (4):
                             Output(4, "to Taco Bell");
+                            wrongResponse = false;
                             break;
                         default:
-                            Console.WriteLine("Please select only a number between 1 and 4");
+                            Console.WriteLine("Please select only a number between 1 and 4\n");
                             continue;
                     }
                 }
                 catch
                 {
-                    Console.WriteLine("Im sorry thats not a valid response! Please only enter a number!");
+                    Console.WriteLine("Im sorry thats not a valid response! Please only enter a number!\n");
                 }
-                wrongResponse = false;
+                
             }
             Console.WriteLine("Thanks for asking! Bye!");
             
         }
         private static void Output(int mood, string activity)
         {
+            bool wrongResponse = true;
             Console.WriteLine("How many people will be with you?");
             int numPeople = Convert.ToInt32(Console.ReadLine());
-            if (numPeople < 1)
+            while (wrongResponse)
             {
-                Console.WriteLine("You should {0} in your sneakers!", activity);
-            }
-            else if (numPeople > 1 && numPeople < 5)
-            {
-                Console.WriteLine("You should {0} in a sedan!", activity);
-            }
-            else if (numPeople > 5 && numPeople < 10)
-            {
-                Console.WriteLine("You should go {0} in a volkswagon bus!", activity);
-            }
-            else if (numPeople > 10)
-            {
-                Console.WriteLine("You should go {0} in an airplane!", activity);
+                if (numPeople < 1 && numPeople > -1)
+                {
+                    Console.WriteLine("You should {0} in your sneakers!", activity);
+                    wrongResponse = false;
+                }
+                else if (numPeople > 1 && numPeople < 5)
+                {
+                    Console.WriteLine("You should {0} in a sedan!", activity);
+                    wrongResponse = false;
+                }
+                else if (numPeople > 4 && numPeople < 10)
+                {
+                    Console.WriteLine("You should go {0} in a volkswagon bus!", activity);
+                    wrongResponse = false;
+                }
+                else if (numPeople > 10)
+                {
+                    Console.WriteLine("You should go {0} in an airplane!", activity);
+                    wrongResponse = false;
+                }
+                else
+                {
+                    Console.WriteLine("Thats not a valid response! Try again please");
+                }
             }
         }
     }
